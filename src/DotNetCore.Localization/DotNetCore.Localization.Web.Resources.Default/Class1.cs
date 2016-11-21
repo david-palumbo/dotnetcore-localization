c8f582af6e4b494baf58836ac20f61f83aebfd80
David Palumbo <david.palumbo@hotmail.com>
2016-11-20 19:59:23 -0500

    Add localization projects.

---
+++ src/DotNetCore.Localization/DotNetCore.Localization.Web.Resources.Default/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore.Localization.Web.Resources.Default
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/DotNetCore.Localization/DotNetCore.Localization.Web.Resources.Default/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("DotNetCore.Localization.Web.Resources.Default")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetCore.Localization.Web.Resources.Default")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("DFC0537E-5C7C-4109-8657-26B20395F910")]
